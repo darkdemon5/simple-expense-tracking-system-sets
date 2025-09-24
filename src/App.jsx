b7f5fb08f7cb129fb5432d99ed13cd305fd5718c
darkdemon5 <yash.sag55@gmail.com>
2025-09-24 14:05:27 +0530

    Changes to be committed: 	new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/assets/SETS.png 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/pages/BackgroundAnimation.jsx 	new file:   src/pages/LoginPage.jsx 	new file:   src/pages/Style.css 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginPage from './Pages/LoginPage'
+
+
+
+const App = () => {
+  return (
+    <div>
+    <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/BackgroundAnimation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Style.css";
+
+const BackgroundAnimation = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default BackgroundAnimation
